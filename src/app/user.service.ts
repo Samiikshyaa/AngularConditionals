76b2747219feb43c00df26a3d1f16320644335ca
Samikshya Timalsina <samikshyat@infinite.com>
2025-02-27 10:58:47 +0545

    feat: github users

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,11 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 
 export interface IUser{
+  // id: number,
+  // name: string,
+  // isActive: boolean
+
   id: number,
-  name: string,
-  isActive: boolean
-  
+  login: string,
+  avatar_url: string
 }
 
 @Injectable({
@@ -13,40 +18,47 @@ export interface IUser{
 })
 export class UserService {
 
-  constructor() { }
-  private users: IUser[] = [
-    {
-      id: 1,
-      name: "ujjwal",
-      isActive: true
-    },
-    {
-      id: 2,
-      name: "prajwal",
-      isActive: false
-    },
-    {
-      id: 3,
-      name: "nawa", 
-      isActive: true
-    }
-  ]
+  constructor(private http: HttpClient){}
+  backendUrl = 'https://api.github.com/users'
+
+  getUsers(): Observable<IUser[]>{
+    const users = this.http.get<IUser[]>(this.backendUrl)
+    return users;
+  }
+  // constructor() { }
+  // private users: IUser[] = [
+  //   {
+  //     id: 1,
+  //     name: "ujjwal",
+  //     isActive: true
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "prajwal",
+  //     isActive: false
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "nawa", 
+  //     isActive: true
+  //   }
+  // ]
 
 
-    getUsers(): IUser[]{
-      return this.users;
-    }
+    // getUsers(): IUser[]{
+    //   return this.users;
+    // }
 
-    getSingleUser(id: number): IUser{
-      const user = this.users.filter(user => user.id === id)
-      if(user.length > 0){
-        return user[0]
-      }
-      return {
-        id: 0,
-        name: "",
-        isActive: false
-      };
-    }
+    // getSingleUser(id: number): IUser{
+    //   const user = this.users.filter(user => user.id === id)
+    //   if(user.length > 0){
+    //     return user[0]
+    //   }
+    //   return {
+    //     id: 0,
+    //     name: "",
+    //     isActive: false
+    //   };
+    // }
   
 }
--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -1,24 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 import { IUser, UserService } from '../user.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-userlist',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './userlist.component.html',
-  styleUrl: './userlist.component.css'
+  styleUrl: './userlist.component.css',
+  providers:[UserService]
 })
-export class UserlistComponent {
+export class UserlistComponent implements OnInit {
 
   userActive: boolean = false
-
+  status = 'Y'
   users: IUser[] = []
 
 
 
   constructor(private userService: UserService){
-      this.users = this.userService.getUsers()
+      // this.users = this.userService.getUsers()
       console.log("constructor called")
   }
 
@@ -33,11 +35,61 @@ export class UserlistComponent {
 // ngAfterViewChecked(){}
 // ngOnDestroy(){}
 
-ngOnInit(){
-  console.log("constructor init")
+// ngOnInit(){
+//   console.log("constructor init")
+// }
+// ngOnChanges(){
+//   console.log("onchange")
+// }
+
+// eventX: EventEmitter<String> = new EventEmitter<String>()
+
+// emmitEvent(){
+//   this.eventX.emit("Hello")
+// }
+
+// observable = new Observable((observer) =>{
+//   observer.next(1)
+//   observer.next(2)
+//   observer.complete()
+// })
+
+ngOnInit(): void {
+  this.userService.getUsers().subscribe(data => {
+     this.users = data
+ })
+ 
 }
-ngOnChanges(){
-  console.log("onchange")
+
+eventX: EventEmitter<string> = new EventEmitter<string>()
+
+emitEvent(){
+ this.eventX.emit("Hello this is event emiting message")
+}
+
+observable = new Observable((observer)=> {
+  observer.next(1)
+  observer.next(2)
+  observer.next(3)
+  observer.complete()
+})
+
+
+fun(){
+ this.observable.subscribe({
+   next: (data) => console.log(data + "from function 1"),
+   complete: () => console.log("Completed"),
+   error: (error) => console.error("error", error)
+ })
+}
+
+
+fun2(){
+ this.observable.subscribe({
+   next: (data) => console.log(data + "from function 2"),
+   complete: () => console.log("Completed"),
+   error: (error) => console.error("error", error)
+ })
 }
 
 
